eccbfe7733e0b1422f874310ce9f2f1910ad0cd2
Fausto G. Cintra <fausto@fgcintra.pro.br>
2020-05-14 10:35:27 -0300

    (14/05) Implementacao de funcao personalizada de comparacao em algoritmos de busca

--- busca.js
+++ busca.js
@@ -2,20 +2,28 @@ let comps
 
 // vet: vetor (universo de busca)
 // busca: valor a ser procurado
-function buscaSequencial(vet, busca) {
+function buscaSequencial(vet, busca, fnComp) {
    for(let i = 0; i < vet.length; i++) {
       comps++
-      if(vet[i] == busca) return i
+      // if(vet[i] == busca) return i
+      if(fnComp(busca, vet[i]) == 0) return i
    }
    // Se não encontrou, retorna -1
    return -1
 }
 
-function buscaBinaria(vet, busca, posIni = 0, posFim = vet.length - 1) {
+// vet: vetor (universo de busca)
+// busca: valor a ser procurado
+// posIni: posição inicial do subvetor de busca (padrão: 0)
+// posFim: posição final do subvetor de busca (padrão: última posição)
+function buscaBinaria(vet, busca, fnComp, posIni = 0, posFim = vet.length - 1) {
    // Math.floor() arredonda o resultado do cálculo para baixo
    const posMeio = Math.floor((posIni + posFim) / 2)
 
-   if(busca == vet[posMeio]) {
+   let resultadoComp = fnComp(busca, vet[posMeio])
+
+   //if(busca == vet[posMeio]) {
+   if(resultadoComp == 0) {
       comps++
       return posMeio // Achou
    }
@@ -23,26 +31,39 @@ function buscaBinaria(vet, busca, posIni = 0, posFim = vet.length - 1) {
       comps += 2
       return -1 // Não existe
    }
-   else if (busca < vet[posMeio]) {
+   else if (resultadoComp < 0) {
       comps += 3
       posFim = posMeio - 1
-      return buscaBinaria(vet, busca, posIni, posFim)
+      return buscaBinaria(vet, busca, fnComp, posIni, posFim)
    }
-   else { // busca > vet[posMeio]
+   else { // fnComp(busca, vet[posMeio]) > 0
       comps += 3
       posIni = posMeio + 1
-      return buscaBinaria(vet, busca, posIni, posFim)
+      return buscaBinaria(vet, busca, fnComp, posIni, posFim)
    }
 }
 
 let nums = [9, 18, 27, 36, 45, 54, 63, 72, 81, 90]
 
-let nomes = require('./amostras/100-mil-nomes')
+//let nomes = require('./amostras/100-mil-nomes')
 
 // Ordenação quick-sort interna do JS
-let nomesOrd = nomes.sort((a, b) => a > b ? 1 : -1)
+//let nomesOrd = nomes.sort((a, b) => a > b ? 1 : -1)
 
-console.log(nomesOrd)
+let objNomes = require('./amostras/100-mil-objetos')
+
+function comparaFirstName(busca, elemVet) {
+   // Uma função de comparação para busca precisa retornar um
+   // dos seguintes valores:
+   // 0, se a == b
+   // (um número positivo, ex. 1), se a > b
+   // (um número negativo, ex. -1) se a < b
+   if(busca == elemVet.first_name) return 0
+   else if (busca > elemVet.first_name) return 1
+   else return -1
+}
+
+/* console.log(nomesOrd)
 
 comps = 0
 console.log(buscaSequencial(nomesOrd, 'WALDISNEY'))
@@ -50,4 +71,18 @@ console.log({comps})
 
 comps = 0
 console.log(buscaBinaria(nomesOrd, 'WALDISNEY'))
+console.log({comps}) */
+
+//console.log(objNomes)
+
+comps = 0
+console.time('sequencial')
+console.log(buscaSequencial(objNomes, 'WALDISNEY', comparaFirstName))
+console.timeEnd('sequencial')
+console.log({comps})
+
+comps = 0
+console.time('binaria')
+console.log(buscaBinaria(objNomes, 'WALDISNEY', comparaFirstName))
+console.timeEnd('binaria')
 console.log({comps})
