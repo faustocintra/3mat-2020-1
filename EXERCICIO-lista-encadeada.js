d70d45cdd1c4c89350af8491e320b31ec6ad10a1
Fausto G. Cintra <fausto@fgcintra.pro.br>
2020-06-18 10:18:50 -0300

    (18/06) Termino de listas encadeadas

---
+++ EXERCICIO-lista-encadeada.js
@@ -0,0 +1,22 @@
+const LinkedList = require('./lib/LinkedList')
+
+/***********************************************
+ * 
+ * 1) Altere a classe LinkedList, acrescentando os métodos:
+ *    - isEmpty(): retorna true se a lista estiver vazia e 
+ *       false caso contrário
+ *    - size(): retorna o tamanho (número de nodos) da lista
+ * 
+ * 2) Crie uma instância da lista encadeada e coloque nela 
+ *    alguns nomes de pessoas. Faça as inserções de modo que
+ *    os nomes fiquem em ordem alfabética:
+ *    - JUREMA
+ *    - ASDRÚBAL
+ *    - EPAMINONDAS
+ *    - SEVERINO
+ *    - BELARMINO
+ *    - MARUSKA
+ *    - WALDISNEY
+ *    - ORESTES
+ */
+ 
